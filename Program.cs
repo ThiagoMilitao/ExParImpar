97a82277a1591e0cb99f57e71493e4709dc067de
thiagoMilitao <thiago.militao2007@gmail.com>
2025-05-12 10:03:13 -0300

    exparimar

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite um número inteiro:");
+int numero = int.Parse(Console.ReadLine()!);
+if (numero % 2 == 0)
+{
+    Console.WriteLine("O número é par.");
+}
+else
+{
+    Console.WriteLine("O número é ímpar.");
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ExParImpar
 Receba um número. Exiba "par" ou "ímpar", conforme o caso.
+
+_Dica: um número é ímpar caso possua resto ao ser dividido por 2._
+
+Ex.:
+
+```
+Digite um número: 17
+17 é ímpar
+```
+
+---
